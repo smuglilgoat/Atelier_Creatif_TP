1c3e8c256668e24b970fd9425e106b1c345d3c59
smuglilgoat <47540165+smuglilgoat@users.noreply.github.com>
2021-02-03 14:54:02 +0100

    corrected

--- Atelier_TP1/atelier_tp1.py
+++ Atelier_TP1/atelier_tp1.py
@@ -2,31 +2,37 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
+import random
+from rich.console import Console
+console = Console()
+
+randX = random.randint(32, 268)
+randY = random.randint(32, 136)
+console.print("Selected zone X : ", randX)
+console.print("Selected zone Y : ", randY)
 
 lbp = cv2.imread("D:\\Documents\\Code\\atelier_creatif_tp\\Atelier_TP1\\lbp.png")
 lbp = cv2.cvtColor(lbp, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Image_LBP',lbp)
-def find_seg():
-    found = 1000
-    x = 0
-    y = 0
-    histo_img = cv2.calcHist([crop_img], [0], None, [256], [0, 256])
-    for i in range(104):
-        for j in range(236):
-            elem = lbp[j:j+64, i:i+64]
-            histo_elem = cv2.calcHist([elem], [0], None, [256], [0, 256])
-            compa = cv2.compareHist(histo_img, histo_elem, cv2.HISTCMP_CHISQR)
-            if(compa < found):
-                found = compa
-                histo_found = histo_elem
-                x = i
-                y = j
-    print("Result X : ", x)
-    print("Result Y : ", y)
-    print("Result correlation : ", found)
-    found_img = lbp[y:y+64, x:x+64]
-    plt.figure(1)
-    plt.imshow(found_img, cmap='gray')
+
+plt.figure('Selected Area')
+select_img = lbp[randY-32:randY+32, randX-32:randX+32]
+plt.imshow(select_img, cmap = 'gray')
+plt.show()
+
+
+def calc_mse(a, b):
+    sum = 0.0
+    for i in range(256):
+        sum = sum + pow((a[i][0] - b[i][0]), 2)
+    return sum / 256
+    
+def compare_hist():
+    histo_ref = cv2.calcHist([select_img], [0], None, [256], [0, 256])
+    # console.print("histo_ref :", histo_ref)
+    # console.print("histo_img :", histo_img)
+    compa = cv2.compareHist(histo_img, histo_ref, cv2.HISTCMP_CHISQR)
+    console.print("Result Comparaison : ", calc_mse(histo_img, histo_ref))
     
 def mouse_click(event,x,y,flags,param):
     global mouseX,mouseY
@@ -34,15 +40,22 @@ def mouse_click(event,x,y,flags,param):
     global crop_img
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        print("MouseX: ", x, "\t| MouseY: ", y)
+        console.print("MouseX: ", x, "\t| MouseY: ", y)
         mouseX,mouseY = x,y
-        crop_img = lbp[y:y+64, x:x+64]
-        histo_img = cv2.calcHist([crop_img], [0], None, [256], [0, 256])
-        cv2.imshow('Cropped',crop_img)
-        plt.figure(0)
-        plt.plot(histo_img)
-        find_seg()
-        plt.show()
+        crop_img = lbp[y-32:y+32, x-32:x+32]
+        if crop_img.shape != (64, 64):
+            console.print(":warning: Selected Area isn't 64x64 image, Please select a correct zone :warning:", style="red on white")
+        else:
+            histo_img = cv2.calcHist([crop_img], [0], None, [256], [0, 256])
+            compare_hist()
+            fig = plt.figure("Selected_Output")
+            ax = fig.add_subplot(1, 2, 1)
+            plt.imshow(crop_img, cmap = 'gray')
+            ax.set_title('Cropped Image')
+            ax = fig.add_subplot(1, 2, 2)
+            plt.plot(histo_img)
+            ax.set_title('Image Histogramme')
+            plt.show()
         
 
 cv2.setMouseCallback('Image_LBP', mouse_click)
@@ -55,4 +68,12 @@ cv2.destroyAllWindows()
 # plt.plot(histo_found)
 # plt.show()
 
+# float MSE(int[] hist1, int[] hist2) {
+#   float somme = 0;
+#   for (int i=0; i < 256; i++) {
+#     somme += Math.pow(hist1[i]-hist2[i], 2);
+#   }
+  
+#   return somme /= 256.0;
+# }
 
